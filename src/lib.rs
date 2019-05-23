3288f4ec93ee9d034cdc2d6e1e69a27dc045de25
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-05-22 19:58:42 -0500

    update

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ pub mod client;
 pub mod errors;
 pub mod structs;
 
+extern crate regex;
+
 #[allow(unused_imports)]
 use structs::enrollments::{
     CreateFaceEnrollmentByUrlReturn, CreateFaceEnrollmentReturn, CreateVideoEnrollmentByUrlReturn,
@@ -40,13 +42,33 @@ use structs::verification::{
     VideoVerificationReturn, VoiceVerificationByUrlReturn, VoiceVerificationReturn,
 };
 
+use regex::Regex;
+
+fn is_user_id(text: &str) -> bool {
+    let re: Regex = Regex::new(r"usr_[a-zA-Z0-9{32}]").unwrap();
+    re.is_match(text)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_basics() {
-        let x = crate::client::VoiceIt2::new(std::env::var("VIAPIKEY").unwrap(), std::env::var("VIAPITOKEN").unwrap());
+        let x = crate::client::VoiceIt2::new(
+            std::env::var("VIAPIKEY").unwrap(),
+            std::env::var("VIAPITOKEN").unwrap(),
+        );
 
-        let result: crate::structs::users::CreateUserReturn = match &x.create_user() {
+        let result: crate::structs::users::GetAllUsers = match &x.get_all_users() {
+            Ok(x) => serde_json::from_str(&x).expect(format!("Unable to unmarshal JSON properly due to call failing and returning with missing values. server response: {}", &x).as_str()),
+            Err(err) => {
+                panic!("Panic error: {:?}", err);
+            }
+        };
+
+        assert_eq!(result.status, 200);
+        assert_eq!(result.responseCode, "SUCC");
+
+        let result: crate::structs::users::CreateUser = match &x.create_user() {
             Ok(x) => serde_json::from_str(&x).expect(&x),
             Err(err) => {
                 panic!("Panic error: {:?}", err);
@@ -55,6 +77,7 @@ mod tests {
 
         assert_eq!(result.status, 201);
         let user_id = result.userId;
+        assert!(crate::is_user_id(&user_id));
         assert_eq!(result.responseCode, "SUCC");
     }
 }
