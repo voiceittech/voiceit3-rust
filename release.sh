5023413b6ab7b9528fdefc6a01003a3ff0460498
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2020-03-05 12:08:49 -0600

    fix release.sh

--- release.sh
+++ release.sh
@@ -103,7 +103,7 @@ then
     fi
 
 
-    curl -u $GITHUBUSERNAME:$GITHUBPASSWORD -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Rust/releases &> /dev/null
+    curl -H "Authorization: token $GH_TOKEN" -H "Content-Type: application/json" --request POST --data '{"tag_name": "'$version'", "target_commitish": "master", "name": "'$version'", "body": "", "draft": false, "prerelease": false}' https://api.github.com/repos/voiceittech/VoiceIt2-Rust/releases &> /dev/null
     if [ "$?" != "0" ]
     then
       curl -X POST -H 'Content-type: application/json' --data '{
