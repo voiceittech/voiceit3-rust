49857c7bd7439197879671248ab434cb6f25466b
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2026-04-05 17:31:56 -0500

    Fix test example (correct method names)

--- examples/test_example.rs
+++ examples/test_example.rs
@@ -1,14 +1,11 @@
-// Test script for VoiceIt3 Rust SDK
-// Run: cargo run --example test_example
-
 use std::env;
-use voiceit3::VoiceIt3;
+use voiceit3::client::VoiceIt3;
 
 fn main() {
-    let api_key = env::var("VOICEIT_API_KEY").expect("Set VOICEIT_API_KEY env var");
-    let api_token = env::var("VOICEIT_API_TOKEN").expect("Set VOICEIT_API_TOKEN env var");
+    let api_key = env::var("VOICEIT_API_KEY").expect("Set VOICEIT_API_KEY");
+    let api_token = env::var("VOICEIT_API_TOKEN").expect("Set VOICEIT_API_TOKEN");
 
-    let vi = VoiceIt3::new(&api_key, &api_token);
+    let vi = VoiceIt3::new(api_key, api_token);
 
     println!("CreateUser: {:?}", vi.create_user());
     println!("GetAllUsers: {:?}", vi.get_all_users());
